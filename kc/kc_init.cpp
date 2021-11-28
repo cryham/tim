4b35eecf45cf41dbadc9b7307159b17c2d4667f8
cryham <cryham@gmail.com>
2021-11-28 21:18:08 +0100

    reduce 3 kc

--- kc/kc_init.cpp
+++ kc/kc_init.cpp
@@ -1,19 +1,9 @@
 #include "kc_data.h"
-#include "kbd_layout.h"
-#include "usb_keyboard.h"
+//#include "usb_keyboard.h"
 #include "matrix.h"
-#include "keys_usb.h"
 #include "kc_params.h"
 #include "WProgram.h"  // rtc, Mouse
 
-
-//------------------------------------------------
-void KC_Main::SeqModClear()
-{
-	//  clear modif
-	for (int i=0; i <= K_ModLast; ++i)
-		seqMod[i] = 0;
-}
 
 //  ctor  Main
 //------------------------------------------------
@@ -21,9 +11,6 @@ KC_Main::KC_Main()
 {
 	err = E_ok;  memSize = 0;
 	setDac = 1;
-	SeqModClear();
-
-	Mouse.screenSize(1920,1200);  //?
 
 	//  rtc set
 	unsigned long t = rtc_get();
@@ -52,77 +39,11 @@ void KC_Main::ResetStats(bool rtc)
 	if (rtc)
 		tm_on = t;
 
-	tm_key = tm_keyOld = tm_keyAct = t;
+	tm_key = tm_keyOld = t;
 	//  ms
 	uint32_t ms = millis();
-	msMin1 = msKeyLay = ms;
 
-	//  cnt
-	tInactSum = tInact1 = tInact2 = 0;
-	min1_Keys = 0;
 
 	cnt_press = 0;  // matrix.h
-	cnt_press1min = 0;
 }
 
-
-//  clear evth
-//------------------------------------------------
-void KC_Setup::Clear()
-{
-	//  header  ver
-	h1 ='k';  h2 = 'c';  ver = 3;  //+ up on changes
-
-	//  default  matrix
-	rows = 8;  cols = 18;  scanKeys = rows * cols;
-	seqSlots = KC_MaxSeqs;
-
-	int i,l;
-	for (l=0; l < KC_MaxLayers; ++l)
-	for (i=0; i < KC_MaxRows * KC_MaxCols; ++i)
-		key[l][i] = KEY_NONE;
-
-	//  const size
-	for (i=0; i < KC_MaxSeqs; ++i)
-	{	seqs[i].data.clear();
-		seqs[i].data.shrink_to_fit();  // free ram
-	}
-}
-
-//  ctor  Setup
-//------------------------------------------------
-KC_Setup::KC_Setup()
-{
-	//Clear();
-	InitCK();
-}
-
-//  Init
-//------------------------------------------------
-void KC_Setup::InitCK()
-{
-	Clear();
-#if defined(CK8)
-	rows = 8;  cols = 20;
-#elif defined(CK1)
-	rows = 6;  cols = 8;
-#elif defined(CK3)
-	rows = 8;  cols = 18;
-#endif
-	scanKeys = rows * cols;
-	seqSlots = KC_MaxSeqs;
-
-	int i;
-	//  from draw layout
-	for (i=0; i < nDrawKeys; ++i)
-	{
-		const DrawKey& dk = drawKeys[i];
-		if (dk.sc != NO)
-		{
-			#define add(code, lay)  key[lay][dk.sc] = code
-			add(dk.code, 0);
-
-			#undef add
-	}	}
-
-}
--- kc/kc_params.h
+++ kc/kc_params.h
@@ -2,22 +2,6 @@
 #include <stdint.h>
 #include "def.h"
 
-/*  data mem sizes
-//--------------------------------------
-  18x8 = 144  * 9L = 1296 B max
-     + 60 seq * 20 = 1200 B
-  = 2500 B  RAM big
-
-  144 keys * (1 size + 2 Lay data) = 432 B
-    + 10 seq * 15 len = 150 B
-  = 600 B  EE small
-*/
-const static int8_t
-	KC_MaxRows = 8,
-	KC_MaxCols = 18,
-	KC_MaxLayers = 8,  // 16
-	KC_MaxSeqs = 60;  // 60
-
 
 //  params, saved
 //--------------------------------------
@@ -31,44 +15,25 @@ struct KC_Params
 	uint8_t scanFreq;
 
 	//  brightness led, 0..100%
-	uint8_t brightness, brightOff;
+	uint8_t brightness;
 	int8_t startScreen;  // screen after start
 
 	uint8_t verCounter;  // inc on each save
 
 	//  gui key auto repeat, ms
 	uint8_t krDelay, krRepeat;
-	uint8_t mkSpeed, mkAccel;  // mouse keys
-
-	//* sequence, default keys delay
-	uint8_t dtSeqDef;
-
-	uint8_t defLayer;   // default layer 0
-	uint8_t editLayer;  // sequence edit layer 2
-	uint8_t keyGui;  // scId for gui toggle
-
-	uint8_t keyMouseSlow;  // mouse shift scId
-	uint8_t mkWhSpeed, mkWhAccel;  // mouse wheel
-	uint8_t quickKeys;  // F1..12 access
-
-	//  layer lock
-	uint8_t msLLTapMax;   // *10ms  max fast press (tap) time to lock layer
-	uint8_t msLLHoldMin;  // *100ms min hold press time to lock layer
 
 	//  ofs
 	int8_t rtcCompensate; // xtal capacitance, adjust ppm
-	uint8_t minInactive;  // minimum minutes for inactive
 	int8_t tempOfs;       // adj Temp val  *0.03'C  +-3.8'C
 
 	//  time intervals
-	uint8_t time1min;     // *6s  press/1min
 	uint8_t timeTemp;     // Temp'C read
 	uint8_t timeTgraph;   // Temp'C add to graph, no average
 	//  Temp graph scale
 	uint8_t minTemp, maxTemp;
 	uint8_t xCur;         // cursor
 
-	#define t1min(par)    (6 * par.time1min)  // s
 	#define tTemp(par)    (100 * gIntervals[par.timeTemp   & gIntvMask])  // ms
 	#define tTgraph(par)  (100 * gIntervals[par.timeTgraph & gIntvMask])
 };
